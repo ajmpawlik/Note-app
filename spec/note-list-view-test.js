4e79c4ae975313fc1e20d104aadc982dd59fad9e
Aleksandra <ajmpawlik@gmail.com>
2019-07-13 19:17:27 +0100

    test passing with html for multiple notes

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -12,3 +12,12 @@ function testView() {
   assert.isTrue(noteListView.getNotesHtmlList() == "<ul><li><div>I like books1</div></li></ul>")
 };
 testView();
+
+function testViewMultipleNotes() {
+  var noteList = new NoteList()
+  noteList.createNote("I like books")
+  noteList.createNote("I like writing code")
+  var noteListView = new NoteListView(noteList)
+  assert.isTrue(noteListView.getNotesHtmlList() == "<ul><li><div>I like books</div></li><li><div>I like writing code</div></li></ul>")
+};
+testViewMultipleNotes();
