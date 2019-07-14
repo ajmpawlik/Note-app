4384caf70568f69896801c46697275cdf3780322
Aleksandra <ajmpawlik@gmail.com>
2019-07-14 18:59:42 +0100

    08, note of 20 characters rreturned

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -7,9 +7,9 @@
 
 function testView() {
   var noteList = new NoteList()
-  noteList.createNote("I like books1")
+  noteList.createNote("I like books, reportages and other non-fiction publications")
   var noteListView = new NoteListView(noteList)
-  assert.isTrue(noteListView.getNotesHtmlList() == "<ul><li><div>I like books1</div></li></ul>")
+  assert.isTrue(noteListView.getNotesHtmlList() == "<ul><li><div>I like books, report</div></li></ul>")
 };
 testView();
 
@@ -19,6 +19,6 @@ function testViewMultipleNotes() {
   noteList.createNote("I like writing code")
   noteList.createNote("I like when my code passes tests I wrote")
   var noteListView = new NoteListView(noteList)
-  assert.isTrue(noteListView.getNotesHtmlList() == "<ul><li><div>I like books</div></li><li><div>I like writing code</div></li><li><div>I like when my code passes tests I wrote</div></li></ul>")
+  assert.isTrue(noteListView.getNotesHtmlList() == "<ul><li><div>I like books</div></li><li><div>I like writing code</div></li><li><div>I like when my code </div></li></ul>")
 };
 testViewMultipleNotes();
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -5,9 +5,8 @@
       var shelf = []
       var resultList1 = "<ul>"
       this.noteList.showAllNotes().forEach(function(item) {
-         shelf.push("<li><div>" + item.text + "</div></li>");
+         shelf.push("<li><div>" + item.text.slice(0, 20) + "</div></li>");
       });
-
       var resultList2 = shelf.join("")
       var resultList3 = "</ul>"
       return resultList1 + resultList2 + resultList3
